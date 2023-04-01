03b3c431df52f77296c3edf535dcf7e589716671
Tim Littlemore <tim@tjl2.com>
2023-04-01 19:42:20 +0100

    Access the OpenAI secret

--- lib/cne-application-cdk-stack.ts
+++ lib/cne-application-cdk-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_lambda as lambda } from 'aws-cdk-lib';
+import { aws_secretsmanager as sm } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -14,11 +15,19 @@ export class CneApplicationCdkStack extends cdk.Stack {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
 
+    const openaiSecret = sm.Secret.fromSecretAttributes(this, "ImportedSecret", {
+      secretCompleteArn:
+        "arn:aws:secretsmanager:eu-west-2:876736351564:secret:OpenAI-API-VP9BIQ"
+    });
+
     // Create the BlackLibrarySYnopsis (Elixir) lambda function
     const lambdaFunction = new lambda.Function(this, 'BlackLibrarySynopsisLambda', {
       runtime: lambda.Runtime.PROVIDED,
       code: lambda.Code.fromAsset('./elixir-lambda.zip'),
-      handler: 'Elixir.BlackLibrarySynopsis:lambda_handler'
+      handler: 'Elixir.BlackLibrarySynopsis:lambda_handler',
+      environment: {
+        OPENAI_API_KEY: openaiSecret.secretValue.unsafeUnwrap().toString()
+      }
     });
   }
 }
